76cfe0eb251a548a1fb3b47757ab5defdb23dc2c
charlotteD06 <paullibelle68@gmail.com>
2026-06-16 19:21:07 +0200

    Rechtliches

--- biblio/src/views/DatenschutzView.vue
+++ biblio/src/views/DatenschutzView.vue
@@ -4,8 +4,46 @@
     <p>
       Diese Web-Applikation verarbeitet nur Beispieldaten im Rahmen eines Hochschulprojekts.
     </p>
-    <p>
-      Es werden keine echten personenbezogenen Daten dauerhaft gespeichert.
+    <p>Verantwortlicher: <br>
+       Marlene Pieper, Charlotte Dietrich <br>
+       E-Mail: charlotte.dietrich06@gmail.com
+    </p>
+    <p>Allgemeines zur Datenverarbeitung: <br>
+       Der Schutz Ihrer personenbezogenen Daten ist uns wichtig. Wir verarbeiten
+       personenbezogene Daten ausschließlich im Rahmen der gesetzlichen Bestimmungen, 
+       insbesondere der Datenschutz-Grundverordnung (DSGVO).
+       Personenbezogene Daten sind alle Informationen, die sich auf eine identifizierte
+       oder identifizierbare Person beziehen.
+    </p>
+    <p>Registrierung und Benutzerkonto: <br>
+       Für die Nutzung bestimmter Funktionen von Biblio ist die Erstellung eines Benutzerkontos erforderlich.
+       Dabei werden folgende Daten verarbeitet: <br>
+       Email, Passwort, Benutzername, Profilbild, persönliche Beschreibung. <br>
+       Die Verarbeitung erfolgt zum Zweck der Bereitstellung und Verwaltung des Benutzerkontos.
+    </p>
+    <p>Persönliche Bibliothek: <br>
+       Nutzer können Bücher ihrer persönlichen Bibliothek hinzufügen und verwalten.
+       Dabei können insbesondere folgende Daten gespeichert werden: <br>
+       gelesene Bücher, Bücher auf der Wunschliste und aktuell gelesene Bücher. <br>
+       Diese Daten werden gespeichert, um die Funktionen der Plattform bereitzustellen.
+    </p>
+    <p>Kommentare und Bewertungen: <br>
+       Wenn Nutzer Kommentare oder Bewertungen veröffentlichen, werden die eingegebenen Inhalte 
+       gespeichert und auf der Plattform angezeigt. Die veröffentlichten Inhalte sind für andere Nutzer sichtbar.
+    </p>
+    <p>Speicherdauer: <br>
+       Personenbezogene Daten werden nur so lange gespeichert, wie dies für die jeweiligen Zwecke erforderlich ist. 
+    </p>
+    <p>Rechte der betroffenen Personen: <br>
+       - Auskunft über gespeicherte Daten <br>
+       - Berichtigung unrichtiger Daten <br>
+       - Löschung personenbezogener Daten <br>
+       - Einschränkung der Verarbeitung <br>
+       - Datenübertragbarkeit <br>
+    </p>
+    <p>Änderungen dieser Datenschutzerklärung: <br>
+       Wir behalten uns vor, diese Datenschutzerklärung anzupassen,
+       wenn dies aufgrund technischer oder rechtlicher Änderungen erforderlich wird.
     </p>
   </main>
 </template>
--- biblio/src/views/ImpressumView.vue
+++ biblio/src/views/ImpressumView.vue
@@ -1,8 +1,17 @@
 <template>
   <main class="container py-5">
     <h1>Impressum</h1>
-    <p>Biblio Web-Applikation</p>
-    <p>Erstellt im Rahmen des Moduls Web Technologien.</p>
-    <p>Kontakt: kontakt@biblio.de</p>
+    <p>Biblio Web-Applikation. Erstellt im Rahmen des Moduls Web Technologien der Hochschule Konstanz.</p>
+    <p>Anschrift der Hochschule: Rheingutstraße, 78462 Konstanz, Germany </p>
+    <p>Verantwortliche des Projekts: </p>
+    <p>Charlotte Dietrich </p>
+    <p>Marlene Pieper </p>
+    <p>Kontakt: charlotte.dietrich06@gmail.com</p>
+    <p>Die Inhalte dieser Webseite wurden mit größtmöglicher Sorgfalt erstellt.
+      Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte übernehmen
+      wir jedoch keine Gewähr.
+      Diese Website kann Links zu externen Websites Dritter enthalten, auf deren Inhalte wir keinen Einfluss haben. 
+      Deshalb können wir für diese fremden Inhalte keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets
+      der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.</p>
   </main>
 </template>
